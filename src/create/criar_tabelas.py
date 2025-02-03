60e6e92b3c8f61b6e54a3e76045bb408882ebc70
CiberS6 <120577674+CiberS6@users.noreply.github.com>
2025-02-03 15:49:51 +0000

    2º

---
+++ src/create/criar_tabelas.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+connection = sqlite3.connect('')
+cursor = connection.cursor()
+
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS picks (
+                id_pick INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome_pick TEXT NOT NULL,
+                id_champion FOREIGN KEY INTEGER NOT NULL,
+                id_roles INTEGER FOREIGN KEY NOT NULL,
+                id_builds INTEGER FOREIGN KEY NOT NULL
+                )
+                ''')
+
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS champions (
+                id_champion INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome_champion TEXT NOT NULL
+                )               
+                ''')
+
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS roles (
+                id_role INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome_role TEXT NOT NULL 
+                )               
+                ''')
+
+cursor.execute('''
+                CREATE TABLE IF NOT EXISTS builds (
+                id_build INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome_build TEXT NOT NULL
+                )               
+                ''')
