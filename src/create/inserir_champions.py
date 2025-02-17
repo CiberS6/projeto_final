55e66f54c67736088e1f5a059a2b2591d1c9042b
CiberS6 <120577674+CiberS6@users.noreply.github.com>
2025-02-17 15:41:12 +0000

    committttttrtgtrtrgibws

--- src/create/inserir_champions.py
+++ src/create/inserir_champions.py
@@ -1,31 +1,43 @@
 import sqlite3
+import sys
+import os
 
-connection = sqlite3.connect('C:\\users\\ba2490\\Desktop\\projeto_final\\projeto_final\\sqlite_database\\lol.db')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..')))
+
+# Current file directory
+current_dir = os.path.dirname(__file__)
+base_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
+db_path = os.path.join(base_dir, 'sqlite_database', 'lol.db')
+
+connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
 
-champions = [
-    "Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe",
-    "Aurelion Sol", "Aurora", "Ambessa Medarda", "Azir", "Bard", "Bel'Veth", "Blitzcrank", "Brand",
-    "Braum", "Briar", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana",
-    "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz",
-    "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger",
-    "Hwei", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx",
-    "K'Sante", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn",
-    "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia",
-    "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Mel",
-    "Milio", "Miss Fortune", "Mordekaiser", "Morgana", "Naafiri", "Nami", "Nasus", "Nautilus",
-    "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu e Willump", "Olaf", "Orianna", "Ornn", "Pantheon",
-    "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renata Glasc",
-    "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine",
-    "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Smolder", "Sona",
-    "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo",
-    "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot",
-    "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear",
-    "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac",
-    "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"
-]
-for champion in champions:
-    cursor.execute("INSERT INTO champions (nome_champion) VALUES (?)", (champion,))
+#champions = [
+#    "Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe",
+#    "Aurelion Sol", "Aurora", "Ambessa Medarda", "Azir", "Bard", "Bel'Veth", "Blitzcrank", "Brand",
+#    "Braum", "Briar", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana",
+#    "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz",
+#    "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger",
+#    "Hwei", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx",
+#    "K'Sante", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn",
+#    "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia",
+#    "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Mel",
+#    "Milio", "Miss Fortune", "Mordekaiser", "Morgana", "Naafiri", "Nami", "Nasus", "Nautilus",
+#    "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu e Willump", "Olaf", "Orianna", "Ornn", "Pantheon",
+#    "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renata Glasc",
+#    "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine",
+#    "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Smolder", "Sona",
+#    "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo",
+#    "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot",
+#    "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear",
+#    "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac",
+#    "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"
+#]
+#for champion in champions:
+#    cursor.execute("INSERT INTO champions (nome_champion) VALUES (?)", (champion,))
 
+nome_champion = input(f"Insira o nome do campeão que quere adicionar: ")
+
+cursor.execute("INSERT INTO champions (nome_champion) VALUES (?)", (nome_champion,))
 connection.commit()
 connection.close()
--- src/read/ler_picks.py
+++ src/read/ler_picks.py
@@ -1,9 +1,18 @@
 import sqlite3
+import sys
+import os
 
-connection = sqlite3.connect('C:\\users\\ba2490\\Desktop\\projeto_final\\projeto_final\\sqlite_database\\lol.db')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..')))
+
+# Current file directory
+current_dir = os.path.dirname(__file__)
+base_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
+db_path = os.path.join(base_dir, 'sqlite_database', 'lol.db')
+
+connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
 
-cursor.execute('SELECT id_pick, id_champion, id_role FROM picks WHERE id_champion = 95')
+cursor.execute('SELECT id_pick, id_champion, id_role FROM picks')
 resultados = cursor.fetchall()
 for champions in resultados:
     print(champions)
