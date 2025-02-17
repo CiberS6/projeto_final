60e0bc6bffe537b1013ae183c960d67da47171e0
Skydave2006 <davidmiguel06pt@gmail.com>
2025-02-17 15:45:55 +0000

    comit talvez

--- src/create/criar_tabelas.py
+++ src/create/criar_tabelas.py
@@ -1,6 +1,15 @@
 import sqlite3
+import sys
+import os
 
-connection = sqlite3.connect('C:\\users\\ba2490\\Desktop\\projeto_final\\projeto_final\\sqlite_database\\lol.db')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..')))
+
+# Current file directory
+current_dir = os.path.dirname(__file__)
+base_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
+db_path = os.path.join(base_dir, 'sqlite_database', 'lol.db')
+
+connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
 
 cursor.execute('''
