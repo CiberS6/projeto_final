22b3daed2a7dd626654eb7d5e62da5a409173a6f
Skydave2006 <davidmiguel06pt@gmail.com>
2025-02-17 11:15:39 +0000

    auto data

--- src/create/inserir_picks.py
+++ src/create/inserir_picks.py
@@ -1,6 +1,16 @@
-import sqlite3
 
-connection = sqlite3.connect('C:\\users\\ba2490\\Desktop\\projeto_final\\projeto_final\\sqlite_database\\lol.db')
+import sqlite3
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..')))
+
+# Current file directory
+current_dir = os.path.dirname(__file__)
+base_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
+db_path = os.path.join(base_dir, 'sqlite_database', 'lol.db')
+
+connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
 
 nome_pick = input("Escreva o nome do pick: ")
