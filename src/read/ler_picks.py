8ca0e9eb0bdf8f7ee345bc64f8120b3b1811c55e
CiberS6 <120577674+CiberS6@users.noreply.github.com>
2025-02-19 09:37:26 +0000

    ler_picks

---
+++ src/read/ler_picks.py
@@ -0,0 +1,20 @@
+import sqlite3
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..')))
+
+# Current file directory
+current_dir = os.path.dirname(__file__)
+base_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
+db_path = os.path.join(base_dir, 'sqlite_database', 'lol.db')
+
+connection = sqlite3.connect(db_path)
+cursor = connection.cursor()
+
+
+def ler_picks():
+    cursor.execute('SELECT id_pick, id_champion, id_roles FROM picks')
+    resultados = cursor.fetchall()
+    for champions in resultados:
+        print(champions)
