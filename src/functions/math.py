5b8dfe7d8163dec8f2e7f10f7e6e1f043055777c
Skydave2006 <davidmiguel06pt@gmail.com>
2025-02-19 10:25:53 +0000

    Update math.py

--- src/functions/math.py
+++ src/functions/math.py
@@ -6,7 +6,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..')))
 
 from src.read.ler_picks import ler_picks 
-from src.read.ler_inner_join import pick_name
+
 # Current file directory
 current_dir = os.path.dirname(__file__)
 base_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
@@ -17,8 +17,6 @@ cursor = connection.cursor()
 
 def winrate():
     print()
-
-    #pick_name()
     ler_picks()
     
 
@@ -31,17 +29,14 @@ def winrate():
     play = 0
 
     for win in win_lose:
-        print(win)
-        
         if win[0] == 1:
             win_count += 1 
         
         play +=1
 
     win_rate = win_count*100/play
-    print(win_count)
     print(f"A winrate de {pick} é {win_rate}")        
 
 
-winrate()
+
 
