39e7739f3b4c8686993923ea2c8fc54cf51f76a1
CiberS6 <120577674+CiberS6@users.noreply.github.com>
2025-02-19 10:55:44 +0000

    ler_picks

--- src/read/ler_picks.py
+++ src/read/ler_picks.py
@@ -1,10 +1,8 @@
-
 import sqlite3
 import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..')))
-
 
 # Current file directory
 current_dir = os.path.dirname(__file__)
@@ -14,11 +12,10 @@ db_path = os.path.join(base_dir, 'sqlite_database', 'lol.db')
 connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
 
-
 def ler_picks():
     cursor.execute('SELECT id_pick, id_champion, id_roles FROM picks')
     resultados = cursor.fetchall()
-
+ 
     cursor.execute('''
                     SELECT picks.nome_pick, champions.nome_champion, roles.nome_role, builds.nome_build
                     FROM picks
@@ -26,11 +23,10 @@ def ler_picks():
                     INNER JOIN roles ON picks.id_roles = roles.id_role
                     INNER JOIN builds ON picks.id_builds = builds.id_build
     ''')
-
+ 
     # Resultados do inner join
     resultados_ = cursor.fetchall()
     for i,champions in zip(resultados_,resultados):
         print(i," ",champions)
-    
-    
-
+ 
+        
