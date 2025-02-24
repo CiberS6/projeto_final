158baef85bd8c95a82451b83a90515c815a715ad
Skydave2006 <davidmiguel06pt@gmail.com>
2025-02-24 15:16:12 +0000

    main update

--- src/app/main.py
+++ src/app/main.py
@@ -6,12 +6,5 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..
 
 from src.functions.menu import menu 
 
-# Current file directory
-current_dir = os.path.dirname(__file__)
-base_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
-db_path = os.path.join(base_dir, 'sqlite_database', 'lol.db')
-
-connection = sqlite3.connect(db_path)
-cursor = connection.cursor()
 
 menu()
